b38cb0292d974603056708570d57c75f23643d63
Jemil <oyebisijemil@gmail.com>
2021-05-12 00:03:09 -0400

    Added solutions remove duplicate from linkedList The first algorithm was added for sorted linked list (the algorithm compare the value neighbouring nodes The second and third algorithms was added for the insorted linked list, the second one uses hashset to keep track of the value of the visited node if it never exists in the hashset and the node is removed once the value was already existed in the hashset (this takes O(n) time complexity The third one uses 2 pointers and for every current node, a runner node traverses through the list and removes a node that  is duplicate

---
+++ DataStructuresAndAlgorithms/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+
+        public Node(int value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
